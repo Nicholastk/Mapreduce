0d458503cfbd0bca837fab4c17fe59fbd06c1bf5
Nicholastk <nicholas-tk@hotmail.com>
2022-05-18 22:01:16 -0300

    Mapreduce

---
+++ src/MapReduceFilterExample/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace MapReduceFilterExample
+{
+    public class Employee
+    {
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+        public decimal Salario { get; set; }
+ 
+
+      public Employee(string nome, string estado, decimal salario)
+        {
+            Nome = nome;
+            Estado = estado;
+            Salario = salario;
+            
+        }
+
+
+        public override string ToString()
+        {
+            return Nome + " " +
+                Estado + " " +
+                Salario;
+                
+        }
+
+    }
+}
---
+++ src/MapReduceFilterExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using MapReduceFilterExample;
+using System.Reflection;
+
+
+List<Employee> lista = new List<Employee>();
+var lines = File.ReadAllLines(@"Employees.txt"); 
+
+
+foreach (var line in lines)
+{
+    if (!string.IsNullOrEmpty(line))
+    {
+        var x = line.Split(";");
+
+        lista.Add(new Employee((x[0]), (x[1]), decimal.Parse(x[2])));
+    }
+    
+}
+
+
+foreach (var item in lista)
+{
+    Console.WriteLine(item);
+}
+
+
+
+Console.Write("MapReduce");
+
+var mapreduce = lista.OrderBy(x => x.Estado).GroupBy(x => x.Estado).Select(x => // mapreduce 
+ new
+ {
+     x.Key,
+     Total = x.Sum(x => x.Salario)
+ });
+
+
+
+foreach (var item in mapreduce)
+{
+    Console.WriteLine(item);
+}
+
+
+
